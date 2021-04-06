f69662eccbcc052833c91ae40644233941cd5113
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-04-06 14:47:14 +0200

    Improve layout

--- TagContainer/TagContainer/ContentView.swift
+++ TagContainer/TagContainer/ContentView.swift
@@ -7,88 +7,123 @@
 
 import SwiftUI
 
-struct Tag: Identifiable {
-    var id: Int
-    var value: String
-}
-
 struct Tags: Identifiable {
-    var id: Int
+    var id = UUID()
     var values: [Tag]
     
-    
+}
+
+struct Tag: Identifiable {
+    var id =  UUID()
+    var value: String
     static let tags = [
-        Tag(id: 1, value: "Covid19"),
-        Tag(id: 2, value: "Technology"),
-        Tag(id: 3, value: "Health"),
-        Tag(id: 4, value: "Auto"),
-        Tag(id: 5, value: "Weather"),
-        Tag(id: 6, value: "Party"),
-        Tag(id: 7, value: "LifeStyle"),
-        Tag(id: 8, value: "Comedy"),
-        Tag(id: 9, value: "Animals"),
-        Tag(id: 10, value: "DIY"),
-        
-        Tag(id: 11, value: "Poverty"),
-        Tag(id: 12, value: "Wealth"),
-        Tag(id: 13, value: "Science & Education"),
-        Tag(id: 14, value: "Music"),
-        Tag(id: 15, value: "Dance"),
-        Tag(id: 16, value: "Human health"),
-        Tag(id: 17, value: "Travel"),
-        Tag(id: 18, value: "Learning"),
-        Tag(id: 19, value: "Sports"),
-        Tag(id: 20, value: "Cinema")
+        Tag(value: "Covid19"),
+        Tag(value: "Technology"),
+        Tag(value: "Health"),
+        Tag(value: "Auto"),
+        Tag(value: "Weather"),
+        Tag(value: "Party"),
+        Tag(value: "LifeStyle"),
+        Tag(value: "Comedy"),
+        Tag(value: "Animals"),
+        Tag(value: "DIY"),
+        Tag(value: "Poverty"),
+        Tag(value: "Wealth"),
+        Tag(value: "Science & Education"),
+        Tag(value: "Music"),
+        Tag(value: "Dance"),
+        Tag(value: "Human health"),
+        Tag(value: "Travel"),
+        Tag(value: "Learning"),
+        Tag(value: "Sports"),
+        Tag(value: "Cinema")
     ]
 }
 struct ContentView: View {
     let size = UIScreen.main.bounds.size
     
     @State private var index: Int = 0
+    @State private var text: String = ""
     
-    @State private var allTags: [Tags] = [Tags(id: 1, values: [])]
+    @State private var allTags: [Tags] = [Tags(values: [])]
     var body: some View {
         ZStack {
             Color(.secondarySystemBackground)
                 .edgesIgnoringSafeArea(.all)
-            VStack(alignment: .leading, spacing: 5)  {
-                ForEach(allTags) { allTag in
-                    HStack(spacing: 5) {
-                        ForEach(allTag.values) { tag in
-                            Button(action: {
+            VStack(alignment: .leading) {
+                VStack(alignment: .leading, spacing: 5)  {
+                    ForEach(allTags) { allTag in
+                        HStack(spacing: 5) {
+                            ForEach(allTag.values) { tag in
+                                Button(action: {
+                                    
+                                }, label: {
+                                    Text(tag.value)
+                                        .foregroundColor(Color(.label))
+                                        .font(.system(size: 10, weight: .semibold))
+                                        .padding(.horizontal, 15)
+                                        .frame(height: 25)
+                                        .background(Color(.systemBackground))
+                                        .clipShape(Capsule())
+                                    
+                                })
                                 
-                            }, label: {
-                                Text(tag.value)
-                                    .foregroundColor(Color(.label))
-                                    .font(.system(size: 10, weight: .semibold))
-                                    .padding(.horizontal, 15)
-                                    .frame(height: 25)
-                                    .background(Color(.systemBackground))
-                                    .clipShape(Capsule())
-                                
-                            })
-                            
+                            }
                         }
                     }
                 }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(10)
+                .background(Color.red)
+                .lineLimit(1)
+                .minimumScaleFactor(0.8)
+                .onAppear(perform: energize)
+                
+                HStack {
+                    TextField("Enter a word", text: $text)
+                    
+                    Button {
+                        let tag = Tag(value: text.trimmingCharacters(in: .whitespaces))
+                        addTag(tag: tag)
+                        text = ""
+                    } label: {
+                        Text("Add")
+                            .foregroundColor(Color(.systemBackground))
+                            .frame(width: 80, height: 32)
+                            .background(Color(.label))
+                    }
+                }
+                .padding(.leading)
+                .frame(height: 32)
+                .background(Color(.systemBackground))
+                .clipShape(Capsule())
+                .padding(10)
             }
-            .padding(10)
-            .lineLimit(1)
-            .minimumScaleFactor(0.8)
-            .onAppear(perform: energize)
         }
     }
     
     private func energize() {
-        for tag in Tags.tags {
+        for tag in Tag.tags {
             let itemSize = allTags[index].values.map(tagsize).reduce(0, +)
             if itemSize < size.width-10 {
                 allTags[index].values.append(tag)
+                
             } else {
                 index += 1
-                allTags.append(.init(id: index+1, values: []))
+                allTags.append(.init(values: []))
                 allTags[index].values.append(tag)
             }
+        }
+    }
+    
+    private func addTag(tag: Tag) {
+        let itemSize = allTags[index].values.map(tagsize).reduce(0, +)
+        if itemSize < size.width-10 {
+            allTags[index].values.append(tag)
+        } else {
+            index += 1
+            allTags.append(.init(values: []))
+            allTags[index].values.append(tag)
         }
     }
     
@@ -120,5 +155,6 @@ extension String {
     func sizeOfString(usingFont font: UIFont) -> CGSize {
         let fontAttributes = [NSAttributedString.Key.font: font]
         return self.size(withAttributes: fontAttributes)
+        re
     }
 }
