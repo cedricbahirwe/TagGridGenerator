b293c6d31c34097df6d45d87c747fcd1e87f4820
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-04-06 15:28:13 +0200

    Make git status git

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # TagGridGenerator
 A layout simulating grid for Text Views in SwiftUI
 
-
-### More Logic Stability will be added in the coming release.
--- TagContainer/TagContainer/ContentView.swift
+++ TagContainer/TagContainer/ContentView.swift
@@ -13,7 +13,7 @@ struct Tags: Identifiable {
     
 }
 
-struct Tag: Identifiable {
+struct Tag: Identifiable, Hashable, Equatable {
     var id =  UUID()
     var value: String
     static let tags = [
@@ -44,8 +44,9 @@ struct ContentView: View {
     
     @State private var index: Int = 0
     @State private var text: String = ""
-    
+    let timer = Timer.publish(every: 0.8, on: .current, in: .common).autoconnect()
     @State private var allTags: [Tags] = [Tags(values: [])]
+    @State private var selectedtags: [Tag] = []
     var body: some View {
         ZStack {
             Color(.secondarySystemBackground)
@@ -56,14 +57,28 @@ struct ContentView: View {
                         HStack(spacing: 5) {
                             ForEach(allTag.values) { tag in
                                 Button(action: {
-                                    
+                                    withAnimation {
+                                        if selectedtags.contains(tag) {
+                                            selectedtags.removeAll(where: { $0.id == tag.id})
+                                        } else {
+                                            selectedtags.append(tag)
+                                        }
+                                    }
                                 }, label: {
                                     Text(tag.value)
-                                        .foregroundColor(Color(.label))
-                                        .font(.system(size: 10, weight: .semibold))
+                                        .foregroundColor(
+                                            selectedtags.contains(tag) ?
+                                            Color(.systemBackground) :
+                                            Color(.label)
+                                        )
+                                        .font(.system(size: 12, weight: .semibold))
                                         .padding(.horizontal, 15)
                                         .frame(height: 25)
-                                        .background(Color(.systemBackground))
+                                        .background(
+                                            selectedtags.contains(tag) ?
+                                            Color(.label) :
+                                            Color(.systemBackground)
+                                        )
                                         .clipShape(Capsule())
                                     
                                 })
@@ -74,61 +89,76 @@ struct ContentView: View {
                 }
                 .frame(maxWidth: .infinity, alignment: .leading)
                 .padding(10)
-                .background(Color.red)
                 .lineLimit(1)
-                .minimumScaleFactor(0.8)
+                .minimumScaleFactor(0.9)
                 .onAppear(perform: energize)
+                
                 
                 HStack {
                     TextField("Enter a word", text: $text)
                     
                     Button {
-                        let tag = Tag(value: text.trimmingCharacters(in: .whitespaces))
-                        addTag(tag: tag)
-                        text = ""
+                        withAnimation {
+                            let tag = Tag(value: text.trimmingCharacters(in: .whitespaces))
+                            addTag(tag: tag)
+                            text = ""
+                        }
+                        
                     } label: {
                         Text("Add")
                             .foregroundColor(Color(.systemBackground))
                             .frame(width: 80, height: 32)
                             .background(Color(.label))
                     }
+                    .disabled(text.trimmingCharacters(in: .whitespaces).count < 3)
                 }
                 .padding(.leading)
                 .frame(height: 32)
                 .background(Color(.systemBackground))
                 .clipShape(Capsule())
                 .padding(10)
+                // Uncomment if you want to see some extra sauce animation
+                .onReceive(timer, perform: animation)
             }
         }
     }
     
     private func energize() {
         for tag in Tag.tags {
-            let itemSize = allTags[index].values.map(tagsize).reduce(0, +)
-            if itemSize < size.width-10 {
-                allTags[index].values.append(tag)
-                
-            } else {
-                index += 1
-                allTags.append(.init(values: []))
-                allTags[index].values.append(tag)
-            }
+            addTag(tag: tag)
         }
     }
     
     private func addTag(tag: Tag) {
         let itemSize = allTags[index].values.map(tagsize).reduce(0, +)
-        if itemSize < size.width-10 {
+        let tagSize = tagsize(tag)
+        let containerWidth = size.width - 20
+        if itemSize + tagSize <= containerWidth {
             allTags[index].values.append(tag)
         } else {
             index += 1
             allTags.append(.init(values: []))
             allTags[index].values.append(tag)
+                
         }
     }
     
     private func tagsize(_ tag: Tag) -> CGFloat {
-        tag.value.widthOfString(usingFont: .systemFont(ofSize: 14, weight: .semibold)) + 55.0
+        let margins: CGFloat = 5.0
+        let paddings: CGFloat = 30
+        return tag.value.widthOfString(usingFont: .systemFont(ofSize: 11, weight: .semibold)) + margins + paddings
+    }
+    
+    private func animation(_ timer: Timer.TimerPublisher.Output) {
+        let mainTags = allTags.map({$0.values}).flatMap({ $0 })
+        let index = Int.random(in: 0..<mainTags.count)
+        withAnimation {
+            if selectedtags.contains(mainTags[index]) {
+                selectedtags.removeAll(where: { $0.id == mainTags[index].id })
+            } else {
+                selectedtags.append(mainTags[index])
+            }
+        }
     }
 }
 
@@ -155,6 +185,6 @@ extension String {
     func sizeOfString(usingFont font: UIFont) -> CGSize {
         let fontAttributes = [NSAttributedString.Key.font: font]
         return self.size(withAttributes: fontAttributes)
-        re
+        
     }
 }
